222fe526e139deb8edfab999ae1c8f8c9134b030
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-28 14:31:04 -0500

    [add] Navigating programatically

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onGotoCategories() {
+    // this.router.navigate(["/categories"]); // it receives a second parameter
+    this.router.navigateByUrl("/categories");
   }
 
 }
